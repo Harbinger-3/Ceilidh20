570d0d06ed5989616c0a0ec247d9745caab39ce2
Aries Harbinger <ariesharbinger3@gmail.com>
2024-12-16 13:37:20 +0800

    Update ceilidh20_file_encryption_decryption.js

--- test/node/ceilidh20_file_encryption_decryption.js
+++ test/node/ceilidh20_file_encryption_decryption.js
@@ -1,11 +1,12 @@
 const fs = require('fs');
 const crypto = require('crypto');
-const { Mash, Alea, toBytes, uintArray, arraySlice, get32, rotl, Ceilidh20_main, Ceilidh20 } = require('./src/ceilidh20');
+const { Mash, Alea, Sha256, toBytes, toChars, uintArray, arraySlice, hexToBytes, get32, rotl, Ceilidh20_main, Ceilidh20 } = require('./src/ceilidh20');
 
 // Generate random key, IV, and nonce
 const key = crypto.randomBytes(32); // 32 bytes key
 const iv = crypto.randomBytes(32);  // 32 bytes IV
 const nonce = crypto.randomBytes(24); // 24 bytes nonce
+const genIVLen = 32;
 
 // Display the key, IV, and nonce
 console.log("Key:", key.toString('hex'));
@@ -18,10 +19,11 @@ const fileData = fs.readFileSync("sample.png");
 // Encrypt the data
 console.time("Encryption Time");
 const encryptedOutput = Ceilidh20(fileData, {
-    key: key,         // 32-byte key
-    iv: iv,           // 32-byte initialization vector
-    nonce: nonce,     // 24-byte nonce
-    isEncrypt: true   // Encrypt binary
+    key: key,           // 32-byte key
+    iv: iv,             // initialization vector
+    nonce: nonce,       // 24-byte nonce
+    genIVLen: genIVLen, // generated IV pair length
+    isEncrypt: true     // Encrypt binary
 });
 console.timeEnd("Encryption Time");
 
@@ -34,10 +36,11 @@ const encryptedFileData = fs.readFileSync("sample.png.encrypted");
 // Decrypt the data
 console.time("Decryption Time");
 const decryptedOutput = Ceilidh20(encryptedFileData, {
-    key: key,         // 32-byte key
-    iv: iv,           // 32-byte initialization vector
-    nonce: nonce,     // 24-byte nonce
-    isEncrypt: false  // Decrypt binary
+    key: key,           // 32-byte key
+    iv: iv,             // initialization vector
+    nonce: nonce,       // 24-byte nonce
+    genIVLen: genIVLen, // generated IV pair length
+    isEncrypt: false    // Decrypt binary
 });
 console.timeEnd("Decryption Time");
 
